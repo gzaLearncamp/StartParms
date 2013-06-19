b5f0957d4d1b724a9179685c73cf280fb04ce94a
Gianni zanetti <voltar99@gmail.com>
2013-06-19 13:01:26 +0200

    first checkin
    hi github, how are you with my first lessons

---
+++ src/StartParms.java
@@ -0,0 +1,21 @@
+
+public class StartParms {
+
+	/** 19 June 2013 gza nach training an der hsr
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Wir zeigen die Startparameter hier");
+			System.out.println(System.getProperty("ApplVersion"));
+			System.out.println("Anzahl Start Parameter:" + args.length);
+			for (int i = 0;i < args.length; i++){
+				System.out.println("Start parm" + (i+1) +" " + args[i]);
+			}
+			
+			
+			
+			
+	}
+
+}
